342767a14ed66c848017e2b3d08b293a20a4f3f6
Akash Verma <akash.verma@capillarytech.com>
2024-02-16 17:19:21 +0530

    add style

---
+++ app/components/pages/ProfilePage/style.js
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+export default css`
+  .heading {
+    color: red;
+    font-style: italic;
+  }
+  .request-cancel-btn {
+    color: white;
+    background-color: red !important;
+  }
+`;
