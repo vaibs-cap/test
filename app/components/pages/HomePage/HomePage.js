fdc71f28ef1bf9a8f2d64d6f05faa32d07e70e9c
Kushagra Nageshwar <kushagra.nageshwar@capillarytech.com>
2024-02-16 12:10:36 +0530

    added template for filter component

--- app/components/pages/HomePage/HomePage.js
+++ app/components/pages/HomePage/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import BookList from '../../organisms/BookList/BookList';
+import Filter from '../../organisms/Filter/Filter';
 
 export const HomePage = () => (
   <>
+    <Filter />
     <BookList />
   </>
 );
