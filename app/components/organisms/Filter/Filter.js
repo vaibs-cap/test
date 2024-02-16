c61d91ec41e93322d35a348d7830fc31018490e4
Kushagra Nageshwar <kushagra.nageshwar@capillarytech.com>
2024-02-16 15:32:48 +0530

    added filter features in ui

--- app/components/organisms/Filter/Filter.js
+++ app/components/organisms/Filter/Filter.js
@@ -1,18 +1,34 @@
 import CapHeader from '@capillarytech/cap-ui-library/CapHeader';
-import React from 'react';
+import React, { Fragment } from 'react';
 import CapCustomSelect from '@capillarytech/cap-ui-library/CapCustomSelect';
+import CapHeading from '@capillarytech/cap-ui-library/CapHeading';
 
 const Filter = () => (
-  <>
-    <CapHeader title="Capillary Library" description="Issue books" />
+  <Fragment>
+    <CapHeader
+      title="Capillary Library"
+      description="Issue books from Capillary Library"
+    />
+    <CapHeading type="h3">Filter Books</CapHeading>
     <CapCustomSelect
-      selectPlaceholder="filter by genre"
+      showSearch
+      selectPlaceholder="Select Genre"
+      value=""
       options={[
-        { label: 'sci-fi', key: 'sci-fi' },
+        { label: 'sci-fi', value: 'sci-fi' },
         { label: 'fiction', value: 'fiction' },
       ]}
     />
-  </>
+    <CapCustomSelect
+      showSearch
+      selectPlaceholder="Select Author"
+      value=""
+      options={[
+        { label: 'author1', value: 'sci-fi' },
+        { label: 'author2', value: 'fiction' },
+      ]}
+    />
+  </Fragment>
 );
 
 export default Filter;
