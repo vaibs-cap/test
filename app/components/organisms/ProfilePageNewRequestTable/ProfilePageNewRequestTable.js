e4bc68e2799cca97a19cb507366c26353523ee03
Akash Verma <akash.verma@capillarytech.com>
2024-02-21 11:42:41 +0530

    default value in new request page added

--- app/components/organisms/ProfilePageNewRequestTable/ProfilePageNewRequestTable.js
+++ app/components/organisms/ProfilePageNewRequestTable/ProfilePageNewRequestTable.js
@@ -6,6 +6,7 @@ import {
   CapButton,
   CapInput,
   CapDatePicker,
+  CapAlert,
 } from '@capillarytech/cap-ui-library';
 import React from 'react';
 import { useState, useEffect } from 'react';
@@ -24,6 +25,7 @@ import bookData from '../../pages/ProfilePage/bookData';
 import * as actions from './actions';
 import { profilePageNewRequestReducer } from './reducer';
 import { makeSelectUserNewBookRequestsData } from './selectors';
+import moment from 'moment';
 
 const ProfilePageNewRequestTable = ({
   className,
@@ -69,7 +71,7 @@ const ProfilePageNewRequestTable = ({
       book_author: tempRow.book_author,
       book_genre: '',
       book_description: '',
-      anticipated_date: '',
+      anticipated_date: moment(),
     }));
     setIsAcceptModalOpen(true);
   };
@@ -83,9 +85,9 @@ const ProfilePageNewRequestTable = ({
   };
 
   const handleAcceptModalAdd = () => {
-    setIsAcceptModalOpen(false);
+    console.log(newReq);
     actions.acceptNewBookRequest(newReq);
-    // console.log(newReq);
+    setIsAcceptModalOpen(false);
   };
 
   const handleAcceptModalCancel = () => {
@@ -180,13 +182,15 @@ const ProfilePageNewRequestTable = ({
             label="Author"
             onChange={handleChange}
           />
-          <CapInput name="book_genre" label="Genre" onChange={handleChange} />
+          <CapInput name="book_genre" label="Genre" value={newReq.book_genre} onChange={handleChange} />
           <CapInput
+            value={newReq.book_description}
             name="book_description"
             label="Description"
             onChange={handleChange}
           />
           <CapDatePicker
+            value = {moment()}
             name="anticipated date"
             label="Ancipated Date"
             onChange={handleDate}
