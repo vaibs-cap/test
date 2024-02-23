c3e129a5385f59a548b45e6927e2f87c0a22172b
Kushagra Nageshwar <kushagra.nageshwar@capillarytech.com>
2024-02-23 16:38:12 +0530

    added minor changes to navbar

--- app/components/organisms/Navbar/Navbar.js
+++ app/components/organisms/Navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = ({ className }) => {
     items: [
       {
         label: 'Capillary Library',
-        link: '/book-list',
+        link: '/',
         key: 'home-page',
       },
       {
--- app/components/organisms/Navbar/styles.js
+++ app/components/organisms/Navbar/styles.js
@@ -5,5 +5,10 @@ export default css`
     background-color: red;
     display: block;
     position: relative;
+    width: 100%;
+  }
+
+  .cap-navbar-menu {
+    width: 100vw;
   }
 `;
